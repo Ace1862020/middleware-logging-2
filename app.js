be7e153a4470aef9fa6d9905afaa587b14c03a3d
Ace1862020 <kyds1862015@gmail.com>
2021-02-03 16:00:40 +0800

    feat: add morgan & get req/res time

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const express = require('express')
+const morgan = require('morgan')
 const port = 3000
 
 const app = express()
+
+
+// Strat
+app.use(function (req, res, next) {
+  req.requestTime = new Date()
+  console.log('requestTime:', req.requestTime)
+  setTimeout(function () {
+    next();
+  }, 3000)
+})
+
+app.use(function (req, res, next) {
+  res.responseTime = new Date()
+  console.log('responseTime:', res.responseTime)
+  next();
+})
+
 
 app.get('/', (req, res) => {
   res.send('列出全部 Todo')
@@ -18,7 +36,6 @@ app.get('/:id', (req, res) => {
 app.post('/', (req, res) => {
   res.send('新增一筆  Todo')
 })
-
 
 app.listen(port, () => {
   console.log(`App running on http://localhost:${port}`)
